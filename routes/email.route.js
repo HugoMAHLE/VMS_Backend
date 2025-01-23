d45cc6178dbc780f14a3f8169795f9ca9794e013
Hugo Gabriel Meza <Hugo.Gabriel.Meza@mahle.com>
2025-01-23 11:04:04 -0700

    Update email.route.js

--- routes/email.route.js
+++ routes/email.route.js
@@ -15,6 +15,9 @@ emailRouter.post('/send', async (req, res) => {
           user: process.env.EMAIL_USER, 
           pass: process.env.EMAIL_PASS,       
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
   });
 
     const mailOptions = {
