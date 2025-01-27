2cd69bd144325817dd3b36c23159929716a4c79d
Hugo Gabriel Meza <Hugo.Gabriel.Meza@mahle.com>
2025-01-26 19:26:13 -0700

    asdw

--- database/email.database.js
+++ database/email.database.js
@@ -1,24 +1,25 @@
 import sql from 'mssql';
-import 'dotenv/config';
 
 const config = {
   user: 'sa',
   password: 'dbaaccess',
-  server: 'mxjc-m2s11srvt1', // Replace with your server's hostname or IP
+  server: 'mxjc-m2s11srvt1',
   database: 'MS2_Report_Services',
   options: {
-    encrypt: true, // Use encryption for data transfer
-    trustServerCertificate: true, // Bypass server certificate validation
+    encrypt: true,
+    trustServerCertificate: true,
   },
 };
 
-try {
-  const pool = await sql.connect(config);
-  console.log('Database connected');
-  const result = await pool.request().query('SELECT GETDATE() AS CurrentDate');
-  console.log(result.recordset);
-} catch (error) {
-  console.error('Database connection failed:', error);
-} finally {
-  sql.close(); // Close the pool to avoid resource leaks
-}
+export const edb = new sql.ConnectionPool(config)
+  .connect()
+  .then((pool) => {
+    console.log('Connected to MSSQL');
+    return pool;
+  })
+  .catch((err) => {
+    console.error('Database Connection Failed! Bad Config: ', err);
+    throw err;
+  });
+
+export { sql };
