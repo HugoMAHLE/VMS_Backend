ac6efca811228f95de78a6fd30934ff94a0911a6
Hugo Gabriel Meza <Hugo.Gabriel.Meza@mahle.com>
2025-01-26 19:18:54 -0700

    batabase sql

--- database/email.database.js
+++ database/email.database.js
@@ -1,20 +1,24 @@
-import pg from 'pg'
-import 'dotenv/config'
+import sql from 'mssql';
+import 'dotenv/config';
 
-const {Pool} = pg
-const connectionString = process.env.EMAILDB_URL
-
-
-export const edb = new Pool({
-  allowExitOnIdle: true,
-  connectionString
-})
+const config = {
+  user: 'sa',
+  password: 'dbaaccess',
+  server: 'mxjc-m2s11srvt1', // Replace with your server's hostname or IP
+  database: 'MS2_Report_Services',
+  options: {
+    encrypt: true, // Use encryption for data transfer
+    trustServerCertificate: true, // Bypass server certificate validation
+  },
+};
 
 try {
-  await edb.query('SELECT NOW()')
-  console.log('Database connected')
+  const pool = await sql.connect(config);
+  console.log('Database connected');
+  const result = await pool.request().query('SELECT GETDATE() AS CurrentDate');
+  console.log(result.recordset);
 } catch (error) {
-  console.log(error)
+  console.error('Database connection failed:', error);
+} finally {
+  sql.close(); // Close the pool to avoid resource leaks
 }
-
-
