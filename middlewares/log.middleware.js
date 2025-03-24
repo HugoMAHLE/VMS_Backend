84c0a039b8143cdef75c3d46f0b124eef2382587
HugoMAHLE <hugo.gabriel.meza@mahle.com>
2025-03-24 12:11:22 -0600

    Update log.middleware.js

--- middlewares/log.middleware.js
+++ middlewares/log.middleware.js
@@ -1,5 +1,8 @@
-import fs from 'fs';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const getLogFilePath = () => {
     const today = new Date().toISOString().split('T')[0]; // Obtiene YYYY-MM-DD
