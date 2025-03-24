51a751ff2d4306415e0c6f7ae31244f399b03107
HugoMAHLE <hugo.gabriel.meza@mahle.com>
2025-03-24 15:45:46 -0600

    Update global.js

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 import { saveLog } from './middlewares/log.middleware.js';
-import {db} from '../database/connection.database.js'
-import {edb, sql} from '../database/email.database.js'
+import {db} from './database/connection.database.js'
+import {edb, sql} from './database/email.database.js'
 
 global.logger = saveLog;
 global.db = db;
