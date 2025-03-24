a2f795556292826bd44c012fca7739a1b3a210b2
HugoMAHLE <hugo.gabriel.meza@mahle.com>
2025-03-24 12:13:51 -0600

    Update log.middleware.js

--- middlewares/log.middleware.js
+++ middlewares/log.middleware.js
@@ -1,5 +1,7 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
