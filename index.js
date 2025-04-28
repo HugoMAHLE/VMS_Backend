2245fdae53360696d1c838bf74140a366378b8c5
HugoMAHLE <hugo.gabriel.meza@mahle.com>
2025-04-28 11:36:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Servidor corriendo en puerto: ' + PORT);
