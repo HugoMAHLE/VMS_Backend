caa2d9de38597968135c16aa366fbbba3c53594b
HugoMAHLE <hugo.gabriel.meza@mahle.com>
2025-04-28 11:33:11 -0600

    port management

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Servidor corriendo en puerto: ' + PORT);
