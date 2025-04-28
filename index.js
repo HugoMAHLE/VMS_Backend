1a065404ead68127fd1e4d2d045cfa98e863c8ed
HugoMAHLE <hugo.gabriel.meza@mahle.com>
2025-04-28 11:48:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/visitor', visitorRouter)
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/visitor', visitorRouter);
 app.use('/api/v1/email', emailRouter);
 app.use('/api/v1/visit', visitRouter);
 
@@ -30,10 +29,19 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
+// Error Handlers for crashes
+process.on('uncaughtException', (err) => {
+  logger('Uncaught Exeption: ' + err.message, "START", "ERROR");
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+  logger('Unhandled Rejection: ' + err.message, "START", "ERROR");
+  process.exit(1);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   logger('Servidor corriendo en puerto: ' + PORT, "START", "STATUS");
 });
-
-
